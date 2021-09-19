676620abdb1de60d4c36d7af136cd71be4392432
aqaqaqz <aqaqaqz@naver.com>
2021-09-19 14:47:10 +0900

    http util 수정
    param 추가

--- api/src/main/java/com/aniserver/api/controller/MainController.java
+++ api/src/main/java/com/aniserver/api/controller/MainController.java
@@ -1,11 +1,16 @@
 package com.aniserver.api.controller;
 
+import com.aniserver.api.model.Directory;
+import com.aniserver.api.util.Util;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.*;
 
 @RestController
 public class MainController {
@@ -21,6 +26,54 @@ public class MainController {
     @GetMapping(value = "/dbTest")
     public List<Map<String, Object>> dbTest() throws Exception {
         return jdbcTemplate.queryForList("SELECT * FROM TEST");
+    }
+
+    @GetMapping(value = "/apiutil")
+    public String apiUtilTest(@RequestParam String keyword) throws IOException {
+        Map<String, String> header = new HashMap<>();
+        header.put("X-Naver-Client-Id", "qxx5tYNz5QnKfeNHRP1H");
+        header.put("X-Naver-Client-Secret", "q3MFVwumGt");
+
+        String url = "https://openapi.naver.com/v1/search/blog?query="+ URLEncoder.encode(keyword, "UTF-8");
+        String rst = Util.http.api(url, "GET", header);
+        JSONObject json = Util.http.convertStringToJson(rst);
+
+        return rst;
+    }
+
+    //https://goddaehee.tistory.com/248 -> 인코딩설정
+
+    @PostMapping(value = "/apiutiltest")
+    public List<Directory> forTest(String input){
+        List<Directory> list = new ArrayList<>();
+        for(int i=0;i<5;i++){
+            Directory d = new Directory();
+            d.setDirectoryId(input + " : id_"+(i+1));
+            d.setName(input + " : name_"+(i+1));
+            list.add(d);
+        }
+        return list;
+    }
+    @GetMapping(value = "/apiutiltest2")
+    public String apiutiltest2(@RequestParam String input) throws IOException {
+        Map<String, String> header = new HashMap<>();
+        //header.put("X-Naver-Client-Id", "qxx5tYNz5QnKfeNHRP1H");
+        //header.put("X-Naver-Client-Secret", "q3MFVwumGt");
+
+        Map<String, String> param = new HashMap<>();
+        param.put("input", URLEncoder.encode(input, "UTF-8"));
+
+        String url = "http://localhost:8080/apiutiltest";
+        String rst = Util.http.api(url, "POST", header, param);
+
+        JSONArray arr = Util.http.convertStringToJsonArray(rst);
+        for(int i=0;i<arr.size();i++){
+            JSONObject json = (JSONObject) arr.get(i);
+            System.out.println("id : " + json.get("directoryId"));
+            System.out.println("name : " + json.get("name"));
+        }
+
+        return rst;
     }
 
 }
--- api/src/main/java/com/aniserver/api/util/utils/HttpUtil.java
+++ api/src/main/java/com/aniserver/api/util/utils/HttpUtil.java
@@ -3,35 +3,69 @@ package com.aniserver.api.util.utils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Map;
 
 public class HttpUtil {
     public HttpURLConnection getConnection(String url, String type, Map<String, String> header) throws IOException {
+        return getConnection(url, type, header, null);
+    }
+    public HttpURLConnection getConnection(String url, String type, Map<String, String> header, Map<String, String> p) throws IOException {
+        boolean isGet = "GET".equals(type);
+        String param = getParamSting(p);
+
+        if(isGet) url += "?"+param;
+
         HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
         connection.setRequestMethod(type);
         for(String key : header.keySet()){
             connection.setRequestProperty(key, header.get(key));
         }
 
-        connection.setDoOutput(false);
+        connection.setDoOutput(true);
+
+        if(!isGet && !StringUtils.isEmpty(param)) {
+            PrintWriter printWriter = new PrintWriter(connection.getOutputStream());
+            printWriter.print(param);
+            printWriter.close();
+        }
+
         return connection;
     }
 
+    public String getParamSting(Map<String, String> param){
+        if(ObjectUtils.isEmpty(param)) return "";
+
+        StringBuffer sb = new StringBuffer();
+
+        for(String key : param.keySet()){
+            if(sb.length()!=0) sb.append("&");
+            sb.append(key + "=" + param.get(key));
+        }
+
+        return sb.toString();
+    }
+
     public String api(String url, String type, Map<String, String> header) throws IOException {
+        return api(url, type, header, null);
+    }
+    public String api(String url, String type, Map<String, String> header, Map<String, String> p) throws IOException {
         String data = "";
 
-        HttpURLConnection connection = getConnection(url, type, header);
+        HttpURLConnection connection = getConnection(url, type, header, p);
 
         StringBuffer sb = new StringBuffer();
         if(connection.getResponseCode() == HttpURLConnection.HTTP_OK){
             BufferedReader br = new BufferedReader(
-                    new InputStreamReader(connection.getInputStream(), "utf-8")
+                new InputStreamReader(connection.getInputStream(), "UTF-8")
             );
 
             String line;
