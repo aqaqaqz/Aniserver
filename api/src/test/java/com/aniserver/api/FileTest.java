2d6e224bf922678d3ca906e866fc1f366893f0ae
Lee SeungHun <aqaqaqz@naver.com>
2021-10-23 20:23:22 +0900

    chore : 테스트코드 작성을 위해 수정
    junit, assertj

---
+++ api/src/test/java/com/aniserver/api/FileTest.java
@@ -0,0 +1,19 @@
+package com.aniserver.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FileTest {
+
+    @BeforeEach
+    void setup(){}
+
+    @Test
+    @DisplayName("junitTest")
+    public void junitTest(){
+        assertThat(true).isTrue();
+    }
+}
