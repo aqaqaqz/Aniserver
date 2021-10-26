f8b1d3f30dac3f781b087cc70c59388266485667
Lee SeungHun <aqaqaqz@naver.com>
2021-10-26 16:48:52 +0900

    test : 타이틀로 분기 구하기 테스트
    폴더 생성 후 2000-1, empty인 경우 테스트코드

---
+++ api/src/test/java/com/aniserver/api/service/DirectoryServiceTest.java
@@ -0,0 +1,39 @@
+package com.aniserver.api.service;
+
+import com.aniserver.api.model.Directory;
+import com.aniserver.api.util.Const;
+import com.aniserver.api.util.Util;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DirectoryServiceTest {
+
+    final static String quarter = "2000-1";
+    final static String existName = "existName";
+    final static String notExistName = "notExistName";
+
+    private static DirectoryService service;
+
+    @BeforeAll
+    static void setup(){
+        service = new DirectoryService();
+        Util.file.makeDirectory(Const.DEFAULT_PATH, quarter);
+        Util.file.makeDirectory(Const.DEFAULT_PATH+"/"+quarter, existName);
+    }
+
+    @AfterAll
+    static void end(){
+        Util.file.removeDirectory(Const.DEFAULT_PATH+"/"+quarter+"/"+existName);
+        Util.file.removeDirectory(Const.DEFAULT_PATH+"/"+quarter);
+    }
+
+    @Test
+    @DisplayName("에피소드 구하기")
+    public void getEpsodeTest(){
+        assertThat(service.getQuarterByTitle(existName)).isEqualTo(quarter);
+        assertThat(service.getQuarterByTitle(notExistName)).isEqualTo(Const.EMPTY);
+    }
+}
