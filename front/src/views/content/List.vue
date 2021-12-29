964ddfe28bf17c0a8072687871108a849b2f8724
Lee SeungHun <aqaqaqz@naver.com>
2021-12-29 13:05:53 +0900

    임시페이지. 깃계정변경 테스트
    임시페이지. 깃계정변경 테스트

---
+++ front/src/views/content/List.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      리스트 임시
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name : "List",
+  data(){
+    return {};
+  }
+})
+</script>
+
+<style scoped></style>
---
+++ front/src/views/content/Player.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    플레이어 임시
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name : "Player",
+  data(){
+    return {};
+  }
+})
+</script>
+
+<style scoped></style>
