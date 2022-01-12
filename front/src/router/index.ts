1f1629c8e0671cd7bea295f3a23f24aefd2681e0
Lee SeungHun <aqaqaqz@naver.com>
2022-01-12 13:00:54 +0900

    리스트 페이지 샘플, 헤더 리스트 이동
    리스트 페이지 샘플, 헤더 리스트 이동

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Main from "../views/main/Main.vue";
 import Login from "../views/login/Login.vue";
+import List from "../views/content/List.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -12,6 +13,12 @@ const routes: Array<RouteRecordRaw> = [
     path: "/login",
     name: "Login",
     component: Login,
+  },
+  {
+    path: "/list/:path",
+    name: "List",
+    component: List,
+    props: true
   },
 ];
 
--- front/src/views/common/Header.vue
+++ front/src/views/common/Header.vue
@@ -6,8 +6,8 @@
           <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
         </a>
 
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0" id="gnbMenu">
+          <li><a href="javascript:void(0);" class="nav-link px-2 text-secondary">Home</a></li>
 
           <li v-for="year in pageData.folderList" v-bind:key="year.name">
             <div class="dropdown">
@@ -16,15 +16,15 @@
               </button>
               <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
                 <li v-for="quarter in year.subList" v-bind:key="quarter.name">
-                  <a class="dropdown-item" href="#">{{quarter.name}}</a>
+                  <a class="dropdown-item path-link" href="javascript:void(0);">{{quarter.name}}</a>
                 </li>
               </ul>
             </div>
           </li>
 
-          <li><a href="#" class="nav-link px-2 text-white">ETC</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">TEMP</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">DOWN</a></li>
+          <li><a href="javascript:void(0);" class="nav-link px-2 text-white path-link">ETC</a></li>
+          <li><a href="javascript:void(0);" class="nav-link px-2 text-white path-link">TEMP</a></li>
+          <li><a href="javascript:void(0);" class="nav-link px-2 text-white path-link">DOWN</a></li>
         </ul>
 
         <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
@@ -33,7 +33,6 @@
 
         <div class="text-end">
           <button type="button" class="btn btn-outline-light me-2" id="loginPageBtn">Login</button>
-          <button type="button" class="btn btn-warning">Sign-up</button>
         </div>
       </div>
     </div>
@@ -56,20 +55,11 @@ export default defineComponent({
       folderList : []
     })
 
-    axios.get('http://localhost:8080/api/directory').then(res => {
-      for(let idx in res.data){
-        let f = res.data[idx];
-        let folder = pageData.folderList.find(folder => folder.name == f.name.substr(0, 4));
-        if(!folder) pageData.folderList.push({name:f.name.substr(0, 4), subList : []}); 
-        
-        pageData.folderList.find(folder => folder.name == f.name.substr(0, 4)).subList.push(f);
-      }
-    })
-
-    onMounted(()=>{
-      let init = getInitFuncs(pageData);
+    let init = getInitFuncs(pageData);
+    onMounted(async ()=>{
       init.initLoginPageBtn();
-    })
+      await init.initGnbMenu();
+    });
 
     return {
       pageData
@@ -77,11 +67,34 @@ export default defineComponent({
   }
 })
 
-function getInitFuncs({router}){
-  return {
-    initLoginPageBtn : function(){
-      console.log(router)
+function getInitFuncs({router, folderList}){
+  let initClickGnbMenu = async function(){
+    for(let li of document.querySelectorAll("#gnbMenu a.path-link")){
+      let path = li.textContent;
+      if(path==null || path=="") continue;
 
+      li.addEventListener("click", (e)=>{
+        router.push({name:'List', params:{path:path}});
+      });
+
+    }
+  }
+
+  return {
+    initGnbMenu : async function (){
+      await axios.get('http://localhost:8080/api/directory').then(async res => {
+        for(let idx in res.data){
+          let f = res.data[idx];
+          let folder = folderList.find(folder => folder.name == f.name.substr(0, 4));
+          if(!folder) folderList.push({name:f.name.substr(0, 4), subList : []}); 
+          
+          folderList.find(folder => folder.name == f.name.substr(0, 4)).subList.push(f);
+        }
+      })
+
+      await initClickGnbMenu();
+    },
+    initLoginPageBtn : function(){
       const loginBtn = document.getElementById("loginPageBtn");
       loginBtn.addEventListener('click', (e)=>{
         router.push('Login');
